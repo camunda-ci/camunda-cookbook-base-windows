9c733561b2d21d96285773f288f8aa470a07f83c
Sebastian Menski <sebastian.menski@googlemail.com>
2015-04-01 14:04:48 +0200

    chore(dns): use FQDN for camunda-ci1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['camunda']['path']['bin'] =  'C:\bin'
 default['camunda']['path']['java'] = 'C:\Program Files\Java'
 
-default['camunda']['host']['ftp'] = 'ftp://camunda-ci1'
+default['camunda']['host']['ftp'] = 'ftp://camunda-ci1.local'
 
 set['camunda']['url']['7zip'] = "#{node['camunda']['host']['ftp']}/ci/binaries/7zip/7z920-x64.msi"
 set['camunda']['url']['sdelete'] = "#{node['camunda']['host']['ftp']}/ci/binaries/sdelete/SDelete.zip"
